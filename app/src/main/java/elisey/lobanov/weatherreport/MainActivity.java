aa19aac4874e409ef0ce8ba4ce590acab0bd8f8d
yeliseyL <crimsonorama@gmail.com>
2020-08-12 19:08:45 +0300

    Initial commit

---
+++ app/src/main/java/elisey/lobanov/weatherreport/MainActivity.java
@@ -0,0 +1,14 @@
+package elisey.lobanov.weatherreport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
